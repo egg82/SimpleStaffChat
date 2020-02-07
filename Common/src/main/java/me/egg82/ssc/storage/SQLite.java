0fa3f760aefc27127d1519645f9ccc1e2288f9a0
egg82 <eggys82@gmail.com>
2020-02-07 11:36:09 -0700

    Update HikariCP, use specific JDBC URLs

--- Common/src/main/java/me/egg82/ssc/storage/SQLite.java
+++ Common/src/main/java/me/egg82/ssc/storage/SQLite.java
@@ -72,6 +72,7 @@ public class SQLite extends AbstractSQL {
             result.handler = handler;
 
             // Baseline
+            config.setDriverClassName("org.sqlite.JDBC");
             config.setConnectionTestQuery("SELECT 1;");
             config.setAutoCommit(true);
             config.addDataSourceProperty("useLegacyDatetimeCode", "false");
